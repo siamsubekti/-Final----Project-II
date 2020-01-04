dbe15f5fee4a1ee3447c7bc9a7c43c5613c93002
tri <tri>
2020-01-04 17:44:55 +0700

    [DOC] able to create deploy.groovy

---
+++ deploy.groovy
@@ -0,0 +1,35 @@
+def sonarProjectKey = "semicolonsquad"
+def repoBackend = 'https://git.enigmacamp.com/enigma-camp/class-b/final-project/kelompok-03/backend.git'
+def credentialsId =  "alfred-pennyworth"
+pipeline {
+    agent any
+    stages {
+        stage('checkout'){
+            steps{
+              checkout([$class: 'GitSCM', branches: [[name: '*master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, url: repoBackend ]]])
+            }
+        }
+        stage('build') {
+            steps {
+                sh "${env.M2_HOME}/bin/mvn clean package"
+            }
+        }
+        stage('sonar') {
+            steps {
+                withCredentials([string(credentialsId: 'sonar-key', variable: 'TOKEN'),string(credentialsId: 'sonar-access', variable: 'ACCESS')]) {
+                   sh "${env.M2_HOME}/bin/mvn sonar:sonar -Dsonar.projectKey=${sonarProjectKey} -Dsonar.host.url=$ACCESS -Dsonar.login=$TOKEN"
+                }
+            }
+        }
+        stage('deploy'){
+            steps{
+                sh "${env.DOCKER_HOME}/docker build -t ged/ged-be:1.0 ."
+            }
+        }
+         stage('docker deploy'){
+            steps{
+                sh "${env.DOCKER_HOME}/docker-compose down && ${env.DOCKER_HOME}/docker-compose up -d"
+            }
+        }
+    }
+}
